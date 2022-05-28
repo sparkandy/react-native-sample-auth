e087e13010cbff093d5c450ba73a1f33426f8359
Andyson <andyson@mypatricia.co>
2022-05-28 10:08:37 +0100

    Sign up and fixed npm bugs

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+
+import { createUser } from '../util/auth';
 
 function SignupScreen() {
-  return <AuthContent />;
+
+  const [isAuthneticating, setIsAuthneticating] = useState(false);
+  
+  async function signupHandler({email, password}){
+    setIsAuthneticating(true);
+    await createUser(email, password);
+    setIsAuthneticating(false);
+  }
+
+  if (isAuthneticating){
+    return <LoadingOverlay message="Creating user..." />
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -2,12 +2,10 @@ import axios from "axios";
 
 const API_KEY = 'AIzaSyBYxJFemCsKJ_pNNSUdA4ghhWarMoBbnHw';
 
-async function createUser (email, password){
+export async function createUser (email, password){
     const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY, {
       email: email,
       password: password,
       returnSecureToken: true  
     });
-
-    
 }
